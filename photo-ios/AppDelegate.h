aadb7b441be68a8b9698bf29b44a6a9563dcfe1f
亮 <liang.wang@wormpex.com>
2018-08-05 12:35:23 +0800

    add

--- photo-ios/AppDelegate.h
+++ photo-ios/AppDelegate.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "GDTSplashAd.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong, nonatomic) GDTSplashAd *splash;
 
 @end
 
